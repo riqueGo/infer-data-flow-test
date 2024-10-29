6ae6e3f27d44db89764ad59110f749b481426fb9
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2024-10-29 19:19:24 -0300

    right

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -4,7 +4,13 @@ public class Assignment {
     public void simple() {
         int x = 10;
         base();
+        int y = x - 5;
+        r(y);
     }
 
     public void base(){}
+
+    public void r(int a) {
+        System.out.println(a);
+    }
 }
